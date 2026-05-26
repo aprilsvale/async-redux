76c37e5f96dfb4f88876ed500714a0a0bf32e8cd
aprilsvale <yanaachurinova2001@gmail.com>
2026-05-26 21:22:18 +0500

    async redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import { fetchPosts } from './features/posts/postsThunk';
+import { fetchPosts } from './features/posts/postsSlice';
 
 
 
